b4e5fc1e1e6e79a53189d624cf9f249ad877e5d7
imajoriri <30540418+imajoriri@users.noreply.github.com>
2018-05-11 13:03:18 +0900

    createObject作成

--- js/index.js
+++ js/index.js
@@ -22,11 +22,42 @@ $(function(){
     attributes.append(makePerStr("attributes", attributesNum));
   });
 
+  //create
+  var createButton = $(".create-button");
+  createButton.on("click", function(){
+    var slots = createObject("slots"); // sltosのkeyとvalueを入れていく
+    var attributes = createObject("attributes"); // attributesのkeyとvalueを入れていく
+    console.log(slots);
+    $("#json-pre").html(JSON.stringify(slots));
+  });
+
 });
 
+// keyとvalueを取得
+function createObject(type){
+  var obj = {};
+  var i = 1;
+  while(true){
+    var perTypeNum = `.per-${type}-${String(i)}`;
+    var typeNumKey = `#${type}-${String(i)}-key`;
+    var typeNumValue = `#${type}-${String(i)}-value`;
+    var key = $(perTypeNum).find(typeNumKey).val(); // key取得
+    var value = $(perTypeNum).find(typeNumValue).val(); // value取得
+    obj[key] = value;
+    if(key === undefined || value === undefined){
+      break;
+    }
+    i++;
+  }
+  return obj;
+}
+
+
+
+// input要素作成
 function makePerStr(type, num){
   num = String(num);
-  var str = "<div class='per-" + type + "'>"
+  var str = "<div class='per-" + type + "-" + num + "'>"
     + "<div class='row'>"
     + "<div class='form-group col-md-5'>"
     + "<input type='text' class='form-control' id='" + type + "-" + num + "-key" + "' placeholder='key'>"
